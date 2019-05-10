7fa087cbb16354a8e2fa24c0d99484ceadffdb87
Julio <28604717+xocolado@users.noreply.github.com>
2019-05-10 15:52:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("Please tell me your mood:")
+
+if mood == "happy":
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times!")
+elif mood == "sad":
+    print("Oh no! Go watch an Adam Sandler movie and you will feel much better.")
+elif mood == "excited":
+    print("That's awesome, mate!")
+elif mood == "relaxed":
+    print("I'm glad to hear that.")
+else:
+    print("I don't recognize this mood.")
